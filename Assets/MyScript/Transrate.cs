a4e8e2e186d44462d9fd15482c266203d1ecdb08
as <>
2021-05-16 05:07:58 +0900

    キャラの配置、エフェクトの実装

---
+++ Assets/MyScript/Transrate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transrate : MonoBehaviour
+{
+    public Vector3 moveObject;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(moveObject * Time.deltaTime);
+    }
+}
